6e2acecf576929431e19c7f48b7b9a7170b86721
jpromanonet <programming@jpromano.net>
2022-02-20 20:10:17 -0300

    POST method

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -6,11 +6,17 @@ use App\Models\Libros;
 
 class LibroController extends Controller{
 
+    // Esta función trae todos los libros de la base de datos.
     public function index(){
 
         $datosLibro = Libros::all();
 
         return response()->json($datosLibro);
+        
+    }
+
+    // Está función guarda datos en MySQL
+    public function guardar(){
         
     }
 
